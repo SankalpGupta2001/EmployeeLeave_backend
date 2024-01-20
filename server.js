02d7775df351b7bce071a74d0658344c835914f4
A2S-Innovate-Tech <83775048+SankalpGupta2001@users.noreply.github.com>
2024-01-20 20:06:34 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const userControllers = require("./controllers/userControllers");
+const leaveControllers = require("./controllers/leaveControllers");
+require('dotenv').config();
+const connectDB = require('./config/db');
+const cors = require('cors');
+
+const app = express();
+app.use(cors({ origin: 'http://localhost:3000' }));
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+connectDB();
+
+app.use('/api', userControllers);
+app.use('/api', leaveControllers);
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
